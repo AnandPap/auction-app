3df2984225e88d347e558fbb3d6c0e02b9583738
Anand Pap <anandpap@live.com>
2025-06-09 16:06:17 +0200

    Added server-side setup.

---
+++ server/src/test/java/com/auctionapp/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auctionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
