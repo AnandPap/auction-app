a8207975e61e51a896be3787cc605e6a719a8d47
Anand Pap <anandpap@live.com>
2025-06-23 07:44:55 +0200

    Added user repository

--- server/src/main/java/com/auctionapp/repository/UserRepository.java
+++ server/src/main/java/com/auctionapp/repository/UserRepository.java
@@ -1,1 +1,14 @@
 package com.auctionapp.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.auctionapp.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
