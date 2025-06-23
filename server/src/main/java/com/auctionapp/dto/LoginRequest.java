d284229866006d9701293f817e054e2e8c92a263
Anand Pap <anandpap@live.com>
2025-06-23 05:56:49 +0200

    Added dtos for auth process

---
+++ server/src/main/java/com/auctionapp/dto/LoginRequest.java
@@ -0,0 +1,6 @@
+package com.auctionapp.dto;
+
+public class LoginRequest {
+    public String email;
+    public String password;
+}
---
+++ server/src/main/java/com/auctionapp/dto/RegisterRequest.java
@@ -0,0 +1,8 @@
+package com.auctionapp.dto;
+
+public class RegisterRequest {
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;    
+}
