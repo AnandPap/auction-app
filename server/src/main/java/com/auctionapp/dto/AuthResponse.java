82a3f7b08a203047de231cce5fbe700fc621201a
Anand Pap <anandpap@live.com>
2025-06-23 07:43:44 +0200

    Added DTOs for login, request and auth.

--- server/src/main/java/com/auctionapp/dto/AuthResponse.java
+++ server/src/main/java/com/auctionapp/dto/AuthResponse.java
@@ -1,9 +1,29 @@
 package com.auctionapp.dto;
 
 public class AuthResponse {
-    public String token;
-    
+
+    private String token;
+    private String error;
+
     public AuthResponse(String token) {
         this.token = token;
+        this.error = null;
+    }
+
+    public AuthResponse(String error, boolean isError) {
+        this.error = error;
+        this.token = null;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public boolean isSuccess() {
+        return token != null;
     }
 }
