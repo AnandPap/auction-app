86cf1eda8165b3639e23969ef192490e88da90d2
Anand Pap <anandpap@live.com>
2025-06-26 10:17:46 +0200

    Fixed default ./ route redirect

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Toast from "../components/Toast";
 import { useAppSelector } from "../redux/hooks";
-import { selectToast } from "../redux/auctionapp";
+import { selectToast, selectAuth } from "../redux/auctionapp";
 
 const Layout = () => {
   const toast = useAppSelector(selectToast);
+  const auth = useAppSelector(selectAuth);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === "/")
+      if (auth.token || auth.isGuest) navigate("/home", { replace: true });
+      else navigate("/login", { replace: true });
+    //eslint-disable-next-line
+  }, []);
 
   return (
     <div className="layout">
